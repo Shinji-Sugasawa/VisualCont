8464466a4232564944c56e0014cf9abca1b4735e
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2020-11-20 12:57:28 +0900

    Update color-change.js

---
+++ kadai-8/color-change.js
@@ -0,0 +1,18 @@
+today = new date();
+
+document.write(today);
+
+switch(today % 4){
+
+    case 0:document.bgColor = "red"
+        break;
+
+    case 1:document.bgColor = "blue"
+        break;
+
+    case 2:document.bgColor = "blue"
+        break
+
+    default:document.bgColor = "black"
+
+}
