fc53cc9785b73077814df81711b03573d0522d39
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2020-11-20 14:37:58 +0900

    Create easy-cal.js

---
+++ kadai-8/8-4/easy-cal.js
@@ -0,0 +1,11 @@
+function multi() {
+
+    var ans;
+    var v1 = document.getElementById("a").value;
+    var v2 = document.getElementById("b").value;
+
+    ans = Number(v1) * Number(v2);
+
+    alert("答えは=" + ans);
+
+}
